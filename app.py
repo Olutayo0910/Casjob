965ea34ba428f4d365370e667bdafa213e3820f6
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-02-01 08:41:29 +0100

    created the register and updated it on the navbar

--- app.py
+++ app.py
@@ -47,17 +47,18 @@ hires = [
 ]
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html', hires=hires)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm
+    form = RegistrationForm()
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
 def login():
-    form = LoginForm
+    form = LoginForm()
     return render_template('login.html', title='Login', form=form)
 
 
